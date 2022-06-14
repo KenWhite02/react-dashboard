c17eb4b44a98f757a095f03ed5cf48733b560fe6
Ken White <whiteken2002@gmail.com>
2022-06-14 08:38:25 +0300

    Renamed file name

--- src/components/index.js
+++ src/components/index.js
@@ -11,7 +11,7 @@ export { default as ThemeSettings } from './ThemeSettings';
 export { default as UserProfile } from './UserProfile';
 
 // Chart Exports
-export { default as StackedLine } from './Charts/StackedLine';
+export { default as SparkLine } from './Charts/SparkLine';
 export { default as LineChart } from './Charts/LineChart';
 export { default as Stacked } from './Charts/Stacked';
 export { default as Pie } from './Charts/Pie';
