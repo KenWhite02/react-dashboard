c62877873757544ed34c4fa200902fd0b970574a
Ken White <whiteken2002@gmail.com>
2022-06-15 09:28:57 +0300

    Introduced props to component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ category, title }) => {
+  return (
+    <div className="mb-10 mt-14 md:mt-0">
+      <p className="text-gray-400">{category}</p>
+      <p className="text-3xl font-extralight -tracking-tight text-slate-900">
+        {title}
+      </p>
+    </div>
+  );
 };
 
 export default Header;
