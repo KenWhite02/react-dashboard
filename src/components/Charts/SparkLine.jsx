ced1ece41295e8c15544398f65429de00421bc7a
Ken White <whiteken2002@gmail.com>
2022-06-15 09:31:23 +0300

    Created chart components

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from '@syncfusion/ej2-react-charts';
 
-const StackedLine = () => {
-  return <div>StackedLine</div>;
+const SparkLine = ({ id, width, height, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      width={width}
+      height={height}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="yval"
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        // eslint-disable-next-line no-template-curly-in-string
+        format: '${x} : data ${yval}',
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
-export default StackedLine;
+export default SparkLine;
