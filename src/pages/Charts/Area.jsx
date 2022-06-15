b18c0109a59b2e9f3a719374d48bc5d44e2bf211
Ken White <whiteken2002@gmail.com>
2022-06-15 12:17:18 +0300

    Created new chart

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  SplineAreaSeries,
+} from '@syncfusion/ej2-react-charts';
+
+import { ChartHeader } from '../../components';
+
+import {
+  areaCustomSeries,
+  AreaPrimaryXAxis,
+  AreaPrimaryYAxis,
+} from '../../data/dummy';
+
+import { useStateContext } from '../../context/ContextProvider';
 
 const Area = () => {
-  return <div>Area</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartHeader category="Chart" title="Area Chart" />
+      <div className="w-11/12">
+        <ChartComponent
+          id="area-chart"
+          height="420px"
+          primaryXAxis={AreaPrimaryXAxis}
+          primaryYAxis={AreaPrimaryYAxis}
+          chartArea={{
+            border: {
+              width: 0,
+            },
+          }}
+          tooltip={{ enable: true }}
+          background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+        >
+          <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+          <SeriesCollectionDirective>
+            {areaCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
 };
 
 export default Area;
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { ChartHeader, LineChart } from '../../components';
 
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartHeader category="Chart" title="Inflation Rate" />
+      <div className="w-11/12">
+        <LineChart />
+      </div>
+    </div>
+  );
 };
 
 export default Line;
