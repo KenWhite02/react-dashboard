540f05a51c0673c0c3643e21c8cde20de657662b
Ken White <whiteken2002@gmail.com>
2022-06-15 12:15:26 +0300

    Removed unused component and import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Navbar, Sidebar, ThemeSettings } from './components';
 import {
   Ecommerce,
   Orders,
