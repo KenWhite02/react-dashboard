68dda1d021659ff23825133c8d00a914a1b92ed7
Ken White <whiteken2002@gmail.com>
2022-06-15 12:27:47 +0300

    Removed unused export

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export { default as Button } from './Button';
 export { default as Cart } from './Cart';
 export { default as ChartHeader } from './ChartHeader';
 export { default as Chat } from './Chat';
-export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Navbar } from './Navbar';
 export { default as Notification } from './Notification';
